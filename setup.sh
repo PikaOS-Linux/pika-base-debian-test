c6cb52fb126d842a847b837deef556f1604d9d73
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-01-12 13:39:27 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -45,4 +45,51 @@ X-Repolib-Default-Mirror: https://www.deb-multimedia.org/
 Signed-By: /etc/apt/keyrings/deb-multimedia-keyring.gpg
 EOF
 
-# Get keys
+# Get keyrings
+mkdir -p /etc/apt/keyrings/
+wget https://github.com/PikaOS-Linux/pkg-pika-sources/blob/main/pika-sources/usr/share/apt-pika/apt/keyrings/pika-keyring.gpg.key -O /etc/apt/keyrings/pika-keyring.gpg.key
+wget https://github.com/CosmicFusion/debian-bleedingedge/raw/main/deb-multimedia-keyring.gpg -O /etc/apt/keyrings/deb-multimedia-keyring.gpg
+
+# Setup apt configration
+mkdir -p  /etc/apt/preferences.d/
+tee /etc/apt/preferences.d/0-pika-debian-settings <<'EOF'
+# Lower Debians's priority under pika's
+Package: *
+Pin: release a=experimental   
+Pin-Priority: 400
+
+
+Package: *
+Pin: release o=Debian
+Pin-Priority: 400
+
+Package: *
+Pin: release o=Unofficial Multimedia Packages
+Pin-Priority: 400
+
+Package: *
+Pin: release a=pikauwu,c=main
+Pin-Priority: 500
+EOF
+
+tee /etc/apt/preferences.d/1-pika-radeon-settings <<'EOF'
+Package: libhsa-runtime64*
+Pin: release o=Debian
+Pin-Priority: 100
+
+Package: hipcc*
+Pin: release o=Debian
+Pin-Priority: 100
+
+Package: rocm*
+Pin: release o=Debian
+Pin-Priority: 100
+
+Package: *
+Pin: release c=rocm
+Pin-Priority: 450
+
+Package: amdgpu-core amdgpu-pro-core amdgpu-dkms amdgpu-pro-lib32
+Pin: release a=*
+Pin-Priority: -10
+EOF
