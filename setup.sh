11286f431d07e5710f39fbc60bbd57455cc91b2c
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-01-12 13:47:02 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,32 +3,33 @@
 mkdir -p /etc/apt/sources.list.d
 
 # Clear /etc/apt/sources.list in favor of deb822 formats
-tee /etc/apt/sources.list.d/system.sources <<'EOF'
+tee /etc/apt/sources.list <<'EOF'
 ## This file is deprecated in PikaOS.
 ## See /etc/apt/sources.list.d/system.sources.
 EOF
 
-# Add Debian Repos
-tee /etc/apt/sources.list.d/system.sources <<'EOF'
-X-Repolib-Name: Debian System Sources
+# Add Debian Repo
+touch /etc/apt/sources.list.d/debian.sources
+tee /etc/apt/sources.list.d/debian.sources <<'EOF'
+X-Repolib-Name: Debian Sources
 Enabled: yes
 Types: deb deb-src
 URIs: http://deb.debian.org/debian
 Suites: sid experimental
 Components: main contrib non-free non-free-firmware
-X-Repolib-ID: system
 X-Repolib-Default-Mirror: http://deb.debian.org/debian
 Signed-by: /usr/share/keyrings/debian-archive-keyring.gpg
 EOF
 
 # Add Pika Repos
-tee /etc/apt/sources.list.d/pika.sources <<'EOF'
-X-Repolib-Name: PikaOS Sources
+tee /etc/apt/sources.list.d/system.sources <<'EOF'
+X-Repolib-Name: PikaOS System Sources
 Enabled: yes
 Types: deb
 URIs: https://ppa.pika-os.com/
 Suites: pikauwu
 Components: main amdgpu rocm
+X-Repolib-ID: system
 X-Repolib-Default-Mirror: https://ppa.pika-os.com/
 Signed-By: /etc/apt/keyrings/pika-keyring.gpg.key
 EOF
